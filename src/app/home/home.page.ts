8dfd678a9113c5f2bc194890d70c43a0a1c3a802
Archeocodix <b0769@hotmail.fr>
2019-11-17 13:45:42 +0100

    add little UI to test wifi direct plugin

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import {Plugins} from '@capacitor/core';
+// import {WifiDirect} from 'capacitor-wifi-direct';
+
+const { WifiDirect } = Plugins;
 
 @Component({
   selector: 'app-home',
@@ -6,7 +10,36 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  wifiState: string;
+  status = 'Sleep';
+  msgs: string;
 
-  constructor() {}
+  msgWriting: string;
 
+  constructor() {
+    WifiDirect.addListener('wifiState', (state: {isEnabled: boolean}) => {
+      this.wifiState = 'Wifi ' + state.isEnabled ? 'on' : 'off';
+    });
+  }
+
+  discoverPeers() {
+    this.status = 'Discovery started';
+    WifiDirect.discoverPeers()
+        .then(({devices}) => {
+          this.status = 'Discovery successfull';
+          console.log(devices);
+        })
+        .catch(err => {
+          console.error(err);
+          this.status = 'Discovery failed';
+        });
+  }
+
+  sendMsg() {
+
+  }
+
+  writeMsg(msg: CustomEvent) {
+    this.msgWriting = msg.detail.value;
+  }
 }
